d97cccadff12d0287e63e5da48d4cee5021c6d5c
Lesly Guadalupe Torres Estrada <146491223+tleslygpe@users.noreply.github.com>
2024-05-27 09:48:07 -0600

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 while True:
+  print("")
   print("Mi calculadora")
   print("1. Sumar")
   print("2. Restar")
@@ -6,44 +7,49 @@ while True:
   print("4. Dividir")
   print("5. Suma Avanzada")
   print("")
-  opcion_seleccionada = input(int("¿Que deseas hacer?: "))
+  opcion_seleccionada = int(input("¿Que deseas hacer?: "))
 
   if opcion_seleccionada == 1:
     print("******* SUMAR *******")
-    numero1 = input(int("Dime el primer número: "))
-    numero2 = input(int("Dime el segundo número: "))
+    numero1 = int(input("Dime el primer número: "))
+    numero2 = int(input("Dime el segundo número: "))
     resultado = sumar(numero1, numero2)
-    print("El resultado es: ", resultado)
-    pass
+    print("*** El resultado es: ", resultado)
 
   if opcion_seleccionada == 2:
     print("******* RESTAR *******")
-    numero1 = input(int("Dime el primer número: "))
-    numero2 = input(int("Dime el segundo número: "))
+    numero1 = int(input("Dime el primer número: "))
+    numero2 = int(input("Dime el segundo número: "))
     resultado = restar(numero1, numero2)
-    print("El resultado es: ", resultado)
-    pass
+    print("*** El resultado es: ", resultado)
     
   if opcion_seleccionada == 3:
     print("******* MULTIPLICAR *******")
-    numero1 = input(int("Dime el primer número: "))
-    numero2 = input(int("Dime el segundo número: "))
+    numero1 = int(input("Dime el primer número: "))
+    numero2 = int(input("Dime el segundo número: "))
     resultado = multiplicar(numero1, numero2)
-    print("El resultado es: ", resultado)
-    pass
+    print("*** El resultado es: ", resultado)
     
   if opcion_seleccionada == 4:
     print("******* DIVIDIR *******")
-    numero1 = input(int("Dime el primer número: "))
-    numero2 = input(int("Dime el segundo número: "))
+    numero1 = int(input("Dime el primer número: "))
+    numero2 = int(input("Dime el segundo número: "))
     resultado = dividir(numero1, numero2)
-    print("El resultado es: ", resultado)
-    pass
+    print("*** El resultado es: ", resultado)
     
   if opcion_seleccionada == 5:
     print("******* SUMA AVANZADA *******")
-    lista = input("Escribe los números a sumar separados por una coma (,): ")
-    resultado = suma_avanzada(numero1, numero2)
-    print("El resultado es: ", resultado)
-    pass
+    lista = []
+    run = True
     
+    while run:
+      respuesta = input("Dime el número que deseas sumar (Presiona Q para terminar la suma): ")
+      if respuesta.lower() == "q":
+        run = False
+        resultado = suma_avanzada(lista)
+        print("*** El resultado es: ", resultado)
+        print("")
+      else:  
+        lista.append(int(respuesta))
+        #print("OK")
+        #print(lista)   
